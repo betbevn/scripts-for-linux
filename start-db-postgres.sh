2422ea794015ca0efd1293d05c799ec5e9c8214d
Hoang Nguyen <royal@Hoangs-MacBook-Pro-3.local>
2022-12-16 08:47:11 +0700

    [royal nguyen] Update

--- start-db-postgres.sh
+++ start-db-postgres.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
-SERVER='vmo-crypto-local';
-PW='g$fKD!sd$101022Lhdev';
-DB='vmo-crypto-local';
+SERVER="vmo-crypto-local";
+USER="vmocryptolocal";
+PW="'gfKDsd101022Lhdev'";
+DB='"vmo-crypto-local"';
 
 echo "echo stop & remove old docker [$SERVER]";
 echo "echo starting new fresh instance of [$SERVER]"
@@ -18,8 +19,21 @@ echo "echo starting new fresh instance of [$SERVER]"
 echo "Waiting for [$SERVER] to start";
 SLEEP 3;
 
-# create the db 
+# create the db
 echo "CREATE DATABASE $DB ENCODING 'UTF-8';" | docker exec -i $SERVER psql -U postgres
-echo "CREATE USER vmocryptodev WITH PASSWORD $PW;" | docker exec -i $SERVER psql -U postgres
-echo "CREATE USER vmocryptodev WITH PASSWORD $PW;" | docker exec -i $SERVER psql -U postgres
+
+# create the user 
+echo "CREATE USER $USER WITH PASSWORD $PW;" | docker exec -i $SERVER psql -U postgres
+echo "ALTER USER $USER WITH LOGIN;" | docker exec -i $SERVER psql -U postgres
+
+# create the grant connect database to user
+echo "GRANT CONNECT ON DATABASE $DB TO $USER;" | docker exec -i $SERVER psql -U postgres
+echo "GRANT ALL PRIVILEGES ON DATABASE $DB to $USER;" | docker exec -i $SERVER psql -U postgres
+
+# another way to execute commnd like echo
+docker exec -i $SERVER psql -U postgres << EOF 
+\c $DB;
+GRANT ALL ON SCHEMA public TO $USER;
+EOF
+
 echo "\l" | docker exec -i $SERVER psql -U postgres
